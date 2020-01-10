38f4be40d8511621d0bd038ce8d61b7f3d70a908
RamonWill <ramonwilliams@hotmail.co.uk>
2020-01-10 21:54:42 +0000

    removed extra loop, updated SQL query string formatting.

--- SuperMarkIt/load_prices.py
+++ SuperMarkIt/load_prices.py
@@ -5,34 +5,36 @@ from scripts import webscrapers as wb
 conn = sqlite3.connect("db.sqlite3")
 conn.row_factory = lambda c, row: row
 c = conn.cursor()
-c.execute("SELECT url_morrisons, url_sainsburys, url_tesco from products_product")
+c.execute("""SELECT url_morrisons, url_sainsburys, url_tesco
+             FROM products_product""")
 database_urls = c.fetchall()
 
-product_urls = []
 
 for rows in database_urls:
     for url in rows:
-        product_urls.append(url)
+        if "morrisons.com" in url:
+            morrisons_price = wb.morrisons_scrape(url)
+            # print(morrisons_price)
+            c.execute("""UPDATE products_product
+                         SET price_morrisons=?
+                         WHERE url_morrisons=?""", (morrisons_price, url))
 
-for url in product_urls:
-    if "morrisons.com" in url:
-        morrisons_price = wb.morrisons_scrape(url)
-        print(morrisons_price)
-        c.execute("UPDATE products_product SET price_morrisons = ? WHERE url_morrisons=?", (morrisons_price, url))
+        elif "sainsburys.co.uk" in url:
+            sainsburys_price = wb.sainsburys_scrape(url)
+            # print(sainsburys_price)
+            c.execute("""UPDATE products_product
+                         SET price_sainsburys = ?
+                         WHERE url_sainsburys=?""", (sainsburys_price, url))
 
-    elif "sainsburys.co.uk" in url:
-        sainsburys_price = wb.sainsburys_scrape(url)
-        print(sainsburys_price)
-        c.execute("UPDATE products_product SET price_sainsburys = ? WHERE url_sainsburys=?", (sainsburys_price, url))
+        elif "tesco.com" in url:
+            tesco_price = wb.tesco_scrape(url)
+            # print(tesco_price)
+            c.execute("""UPDATE products_product
+                         SET price_tesco=?
+                         WHERE url_tesco=?""", (tesco_price, url))
 
-    elif "tesco.com" in url:
-        tesco_price = wb.tesco_scrape(url)
-        print(tesco_price)
-        c.execute("UPDATE products_product SET price_tesco = ? WHERE url_tesco=?", (tesco_price, url))
-
-    else:
-        print("FLAG: Is the following url correct? {}".format(url))
-
+        else:
+            print("FLAG: Is the following url correct? {}".format(url))
 
 conn.commit()
 c.close()
